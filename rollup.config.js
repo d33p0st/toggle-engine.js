80390923346fb9c4e5fe78f080dff630730a0b67
d33p0st <d33p0st@gmail.com>
2025-01-19 02:27:28 +0530

    Update: Using rollup/plugin-terser instead of babel and webpack

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+// rollup.config.js
+import terser from '@rollup/plugin-terser'; // Correctly import the terser plugin
+
+export default {
+  input: 'src/index.js',  // Entry file
+  output: {
+    file: 'dist/main.min.js',  // Output minified file
+    format: 'iife',  // Immediately Invoked Function Expression (IIFE) for browser compatibility
+    name: 'enableDarkMode',  // Name of the global variable if needed in the browser
+  },
+  plugins: [
+    terser()  // Correct usage of the terser plugin for minification
+  ]
+};
