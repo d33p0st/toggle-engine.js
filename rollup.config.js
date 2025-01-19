67f8ab21a14b056c785f74384cc127841325e330
d33p0st <d33p0st@gmail.com>
2025-01-19 20:23:14 +0530

    Udpate

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
-// rollup.config.js
-import terser from '@rollup/plugin-terser'; // Correctly import the terser plugin
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/index.js',  // Entry file
-  output: {
-    file: 'dist/dark-mode.min.js',  // Output minified file
-    format: 'iife',  // Immediately Invoked Function Expression (IIFE) for browser compatibility
-    name: 'enableDarkMode',  // Name of the global variable if needed in the browser
-  },
-  plugins: [
-    terser()  // Correct usage of the terser plugin for minification
-  ]
+    input: 'src/index.js',
+    output: [
+        {
+            file: 'dist/dark-mode.min.js',
+            format: 'iife',
+            name: 'MyLib', // Global variable name
+            plugins: [terser()]
+        },
+        {
+            file: 'dist/dark-mode.esm.js',
+            format: 'esm'
+        }
+    ],
+    plugins: [resolve(), commonjs()]
 };
